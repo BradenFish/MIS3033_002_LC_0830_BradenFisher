5a46e03d038f5584c96d00c8b28b8449f4e11891
Braden Fisher <Braden.J.Fisher-1@ou.edu>
2023-08-29 10:55:36 -0500

    Add project files.

---
+++ MIS3033_002_LC_0830_YourName/Program.cs
@@ -0,0 +1,15 @@
+﻿// MIS 3033 002
+// August 30, 2023
+// Braden Fisher
+
+string str1; // complex
+str1 = new string("MIS3033");
+str1 = @"M\nIS\t3033"; // line 6 and 7 are the same
+str1 = "MIS3033BradenFisher";
+
+int l;
+l = str1.Length;
+Console.WriteLine(l);
+
+Console.WriteLine(str1.ToLower());
+Console.WriteLine(str1.Contains("MIS"));
